bdcde6cd7f787b74cf732e1b620883625100bd52
Павел Желнов <pzhelnov@p1m.org>
2019-04-29 09:03:38 +0300

    Переместить в ожидающие не исключенные только ЭБД
    Оказалось, что при экспорте из Rayyan добавились
    также и такие записи (ПВЖ: исключить; ЭБД:
    не исключать; САС: не проскринено).
    То есть пустые значения в случае САС Рэйан расценил
    как истинные.
    
    Соответственно, того же следует ожидать и в списке:
    ПВЖ: не исключать; ЭБД:
    исключить; САС: не проскринено.
    
    Это нам даже на руку, потому что это значит, что в лице 146
    мы получили максимум возможных преконсенсусных вариантов,
    то есть по завершении скрининга САС часть из этих
    146 просто уйдут.
    
    По технике: выявил такие записи (41 запись),
    написал запросы на их автопомещение
    в ожидающие рассмотрения,
    выполнил.
    
    Все файлы добавляю, в том числе таблицу columns_metadata
    из БД для ясности.
    
    В списке 41 оставил только поля key и notes
    (остальные — незачем).
    
    Теперь то же проделаю с scr_1_pvz_not_excluded_sas_na.

---
+++ scr_1_ebd_not_excluded_sas_na_41.sql
@@ -0,0 +1,35 @@
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 9 - НСО или НРИ [Records Excluded Step 9a NSR or NRS]', c159=3
+where c21=14628199 OR c21=11880843 OR c21=11148899 OR c21=10549705 OR c21=10472098 OR c21=9637621 OR c21=9460160 OR c21=8913955 OR c21=8814573 OR c21=7479408 OR c21=8178177 OR c21=8300651 OR c21=7874541 OR c21=8237236 OR c21=1553331 OR c21=1785100 OR c21=28323717 OR c21=28107241;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 1 - фаза 0-1 [Records Excluded Step 1 Animal or Cadaver or Biomech or In Vitro or In Silico or Healthy]', c159=3
+where c21=1439861 OR c21=27135451 OR c21=18377319 OR c21=19404687 OR c21=27005301 OR c21=25813011 OR c21=14634854;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 8a - не та хирургия [Records Excluded Step 8a Surgery Not Directly Relevant]', c159=3
+where c21=28247080 OR c21=9882919 OR c21=27646696 OR c21=9755762 OR c21=9051896 OR c21=20189171;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 8 - не хирургия [Records Excluded Step 8 No Surgery in the Spine]', c159=3
+where c21=26306404 OR c21=25457338;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 7 - ВП [Records Excluded Step 7 VA or RFA]', c159=3
+where c21=24845086;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 6b - последствия травм [Records Excluded Step 6b Late TL Trauma]', c159=3
+where c21=19412024 OR c21=25953526;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 6 - осложненная травма [Records Excluded Step 6 TL Trauma SCI Present]', c159=3
+where c21=21604529 OR c21=18040702;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 5c - не травма [Records Excluded Step 5c TL Spine Other Than Trauma]', c159=3
+where c21=22366970;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 4a - шея [Records Excluded Step 4a Adult C-Spine]', c159=3
+where c21=29477004
