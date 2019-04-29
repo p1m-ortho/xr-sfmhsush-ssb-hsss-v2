53978aa58b409260e27d64ee0e9a9d21cdb9ecad
Павел Желнов <pzhelnov@p1m.org>
2019-04-29 09:49:01 +0300

    Переместить в ожидающие не исключенные только ПВЖ
    Выявил 3 таких записи.
    
    Все в соответствии с bdcde6cd7f787b74cf732e1b620883625100bd52.
    
    Больше таких записей, в том числе в оставшихся 2 CSV-списках, нет.
    Остальные все не исключены, как минимум, двумя скринерами.

---
+++ scr_1_pvz_not_excluded_sas_na_3.sql
@@ -0,0 +1,7 @@
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 8a - не та хирургия [Records Excluded Step 8a Surgery Not Directly Relevant]', c159=3
+where c21=12810974 OR c21=7817241;
+
+update t2
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 6 - осложненная травма [Records Excluded Step 6 TL Trauma SCI Present]', c159=3
+where c21=11172655
