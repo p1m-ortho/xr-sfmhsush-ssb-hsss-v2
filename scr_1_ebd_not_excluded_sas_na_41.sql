d37d693cf56f5d042137acb0851e073a0ba6b7eb
Павел Желнов <pzhelnov@p1m.org>
2019-04-30 22:30:36 +0300

    Пояснить про `c159=3` в запросах

--- scr_1_ebd_not_excluded_sas_na_41.sql
+++ scr_1_ebd_not_excluded_sas_na_41.sql
@@ -1,5 +1,5 @@
 update t2
-set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 9 - НСО или НРИ [Records Excluded Step 9a NSR or NRS]', c159=3
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 9 - НСО или НРИ [Records Excluded Step 9a NSR or NRS]', c159=3 /* 3 — Ожидают рассмотрения (1 — Включены; 2 — Исключены) */
 where c21=14628199 OR c21=11880843 OR c21=11148899 OR c21=10549705 OR c21=10472098 OR c21=9637621 OR c21=9460160 OR c21=8913955 OR c21=8814573 OR c21=7479408 OR c21=8178177 OR c21=8300651 OR c21=7874541 OR c21=8237236 OR c21=1553331 OR c21=1785100 OR c21=28323717 OR c21=28107241;
 
 update t2
--- scr_1_pvz_not_excluded_sas_na_3.sql
+++ scr_1_pvz_not_excluded_sas_na_3.sql
@@ -1,5 +1,5 @@
 update t2
-set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 8a - не та хирургия [Records Excluded Step 8a Surgery Not Directly Relevant]', c159=3
+set c138='RAYYAN-INCLUSION: {"Pavel"=>false, "dursunov.eldar97"=>true} | RAYYAN-EXCLUSION-REASONS: шаг 8a - не та хирургия [Records Excluded Step 8a Surgery Not Directly Relevant]', c159=3  /* 3 — Ожидают рассмотрения (1 — Включены; 2 — Исключены) */
 where c21=12810974 OR c21=7817241;
 
 update t2
